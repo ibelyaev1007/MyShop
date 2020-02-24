a938847e111c22758768c38012af0de4f9d182d6
Ivan <ivanbeljaev@mail.ru>
2020-02-24 18:13:47 +0500

    Added Models

---
+++ Shop/Data/Interfaces/IAllCars.cs
@@ -0,0 +1,17 @@
+﻿using Shop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data.interfaces
+{
+    interface IAllCars
+    {
+        IEnumerable<Car> Cars { get; }
+
+        IEnumerable<Car> getFavsCars { get; set; }
+
+        Car getObjectCar(int carID);
+    }
+}
---
+++ Shop/Data/Mocks/MockCars.cs
@@ -0,0 +1,41 @@
+﻿using Shop.Data.interfaces;
+using Shop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Mocks
+{
+    public class MockCars : IAllCars
+    {
+        private readonly ICarsCategory _categoryCars = new MockCategory();
+        public IEnumerable<Car> Cars 
+        {
+            get 
+            {
+                return new List<Car>
+                {
+                    new Car 
+                    { 
+                        name = "Tesla Model S", 
+                        shortDesc = "Быстрый автомобиль", 
+                        img = "https://upload.wikimedia.org/wikipedia/commons/4/4f/Tesla_Model_S_02_2013.jpg", 
+                        longDesc = "Красивый, быстрый и очень тихий автомобиль компании Telsa", 
+                        price = 45000, 
+                        isFavourite = true, 
+                        available = true, 
+                        Category = _categoryCars.AllCagegories.First() 
+                    }                    
+                };
+            }
+                
+        }
+        public IEnumerable<Car> getFavsCars { get; set; }
+
+        public Car getObjectCar(int carID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
