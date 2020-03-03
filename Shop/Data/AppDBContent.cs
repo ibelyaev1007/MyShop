37c80ceaeaf5de83376afc7db03de3089cfce820
Ivan Belyaev <Ivan.Belyaev@emerson.com>
2020-03-03 15:43:54 +0500

    Added DB

---
+++ Shop/Data/AppDBContent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Shop.Data.Models;
+
+namespace Shop.Data
+{
+    public class AppDBContent : DbContext
+    {
+        public AppDBContent(DbContextOptions<AppDBContent> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Car> Car { get; set; }
+        public DbSet<Car> Category { get; set; }
+    }
+}
--- Shop/Startup.cs
+++ Shop/Startup.cs
@@ -4,10 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Shop.Data;
 using Shop.Data.interfaces;
 using Shop.Data.Mocks;
 
@@ -15,10 +19,18 @@ namespace Shop
 {
     public class Startup
     {
+        private IConfigurationRoot _confString;
+
+        public Startup(IWebHostEnvironment hostEnv)
+        {
+            _confString = new ConfigurationBuilder().SetBasePath(hostEnv.ContentRootPath).AddJsonFile("dbsettings.json").Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDBContent>(options => options.UseSqlServer(_confString.GetConnectionString("DefaultConnection")));
             services.AddTransient<IAllCars, MockCars>();
             services.AddTransient<ICarsCategory, MockCategory>();
             services.AddMvc();
