abfe7f4461b64ec46999d26e67197025f920173a
Ivan Belyaev <Ivan.Belyaev@emerson.com>
2020-03-10 08:10:20 +0500

    Added Home Page

---
+++ Shop/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Data.interfaces;
+using Shop.Data.Models;
+using Shop.ViewModels;
+
+namespace Shop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAllCars _carRep;
+
+        public HomeController(IAllCars carRep)
+        {
+            _carRep = carRep;
+        }
+
+        public ViewResult Index()
+        {
+            var homeCars = new HomeViewModel {favCars = _carRep.getFavsCars};
+            return View(homeCars);
+        }
+    }
+}
